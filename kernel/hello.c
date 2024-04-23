925b257e5fc48e642e82d1cb871f8046319bd6e2
enen <hzhanzheng@gmail.com>
2024-04-23 22:13:45 +0200

    1

--- kernel/hello.c
+++ kernel/hello.c
@@ -1,3 +1,5 @@
+#include "linux/hw_breakpoint.h"
+#include "linux/perf_event.h"
 #include "linux/pid.h"
 #include "linux/sched/task.h"
 #include "linux/types.h"
@@ -21,14 +23,27 @@
 
 #define DEVICE_NAME "kernel_hack"
 
-//extern struct mm_struct *get_task_mm(struct task_struct *task);
-
 static inline unsigned long size_inside_page(unsigned long start,
 					     unsigned long size)
 {
 	unsigned long sz;
 	sz = PAGE_SIZE - (start & (PAGE_SIZE - 1));
 	return min(sz, size);
+}
+
+static inline struct task_struct *my_get_task_struct_by_pid(pid_t pid)
+{
+	struct pid *pid_struct;
+	struct task_struct *task;
+
+	pid_struct = find_get_pid(pid);
+	if (!pid_struct)
+		return NULL;
+
+	task = get_pid_task(pid_struct, PIDTYPE_PID);
+	put_pid(pid_struct);
+
+	return task;
 }
 
 static phys_addr_t translate_linear_address(struct mm_struct *mm, uintptr_t va)
@@ -75,7 +90,6 @@ static phys_addr_t translate_linear_address(struct mm_struct *mm, uintptr_t va)
 static size_t read_process_memory(pid_t pid, uintptr_t addr, void *buffer,
 				  size_t size)
 {
-	struct pid *pid_struct;
 	struct task_struct *task;
 	struct mm_struct *mm;
 	phys_addr_t pa;
@@ -84,13 +98,9 @@ static size_t read_process_memory(pid_t pid, uintptr_t addr, void *buffer,
 	char *bounce;
 	int probe;
 
-	pid_struct = find_get_pid(pid);
-	if (!pid_struct)
-		return 0;
-
-	task = get_pid_task(pid_struct, PIDTYPE_PID);
+	task = my_get_task_struct_by_pid(pid);
 	if (!task)
-		goto put_pid;
+		return 0;
 
 	mm = get_task_mm(task);
 	if (!mm || IS_ERR(mm))
@@ -135,15 +145,12 @@ mmput:
 	mmput(mm);
 put_task_struct:
 	put_task_struct(task);
-put_pid:
-	put_pid(pid_struct);
 	return read;
 }
 
 static size_t write_process_memory(pid_t pid, uintptr_t addr, void *buffer,
 				   size_t size)
 {
-	struct pid *pid_struct;
 	struct task_struct *task;
 	struct mm_struct *mm;
 	phys_addr_t pa;
@@ -151,13 +158,9 @@ static size_t write_process_memory(pid_t pid, uintptr_t addr, void *buffer,
 	void *ptr;
 	unsigned long copied;
 
-	pid_struct = find_get_pid(pid);
-	if (!pid_struct)
-		return 0;
-
-	task = get_pid_task(pid_struct, PIDTYPE_PID);
+	task = my_get_task_struct_by_pid(pid);
 	if (!task)
-		goto put_pid;
+		return 0;
 
 	mm = get_task_mm(task);
 	if (!mm || IS_ERR(mm))
@@ -193,14 +196,11 @@ static size_t write_process_memory(pid_t pid, uintptr_t addr, void *buffer,
 	mmput(mm);
 put_task_struct:
 	put_task_struct(task);
-put_pid:
-	put_pid(pid_struct);
 	return written;
 }
 static size_t read_process_memory2(pid_t pid, uintptr_t addr, void *buffer,
 				   size_t size)
 {
-	struct pid *pid_struct;
 	struct task_struct *task;
 	struct mm_struct *mm;
 	phys_addr_t pa;
@@ -209,13 +209,9 @@ static size_t read_process_memory2(pid_t pid, uintptr_t addr, void *buffer,
 	char *bounce;
 	int probe;
 
-	pid_struct = find_get_pid(pid);
-	if (!pid_struct)
-		return 0;
-
-	task = get_pid_task(pid_struct, PIDTYPE_PID);
+	task = my_get_task_struct_by_pid(pid);
 	if (!task)
-		goto put_pid;
+		return 0;
 
 	mm = get_task_mm(task);
 	if (!mm || IS_ERR(mm))
@@ -257,15 +253,12 @@ mmput:
 	mmput(mm);
 put_task_struct:
 	put_task_struct(task);
-put_pid:
-	put_pid(pid_struct);
 	return read;
 }
 
 static size_t write_process_memory2(pid_t pid, uintptr_t addr, void *buffer,
 				    size_t size)
 {
-	struct pid *pid_struct;
 	struct task_struct *task;
 	struct mm_struct *mm;
 	phys_addr_t pa;
@@ -273,13 +266,9 @@ static size_t write_process_memory2(pid_t pid, uintptr_t addr, void *buffer,
 	void *ptr;
 	unsigned long copied;
 
-	pid_struct = find_get_pid(pid);
-	if (!pid_struct)
-		return 0;
-
-	task = get_pid_task(pid_struct, PIDTYPE_PID);
+	task = my_get_task_struct_by_pid(pid);
 	if (!task)
-		goto put_pid;
+		return 0;
 
 	mm = get_task_mm(task);
 	if (!mm || IS_ERR(mm))
@@ -312,10 +301,180 @@ static size_t write_process_memory2(pid_t pid, uintptr_t addr, void *buffer,
 	mmput(mm);
 put_task_struct:
 	put_task_struct(task);
-put_pid:
-	put_pid(pid_struct);
 	return written;
 }
+
+#define MAX_HBP_COUNT 300
+#define MAX_INDEX_COUNT 50
+
+#pragma pack(1)
+struct My_User_Regs_struct {
+	__uint128_t vregs[32];
+	u32 fpsr;
+	u32 fpcr;
+
+	u64 xregs[31];
+	u64 sp;
+	u64 pc;
+	u64 pstate;
+
+	bool vregs_set[32];
+	bool fpsr_set;
+	bool fpcr_set;
+	bool xregs_set[31];
+	bool sp_set;
+	bool pc_set;
+	bool pstate_set;
+};
+
+struct My_HW_struct {
+	struct perf_event *hbp[MAX_HBP_COUNT];
+	struct My_User_Regs_struct r;
+};
+#pragma pack()
+
+struct My_HW_struct my_hw_struct[MAX_INDEX_COUNT];
+
+static int current_index = 0;
+
+static void sample_hbp_handler(struct perf_event *bp,
+			       struct perf_sample_data *data,
+			       struct pt_regs *regs)
+{
+	int i;
+	bool do_vregs_set = false;
+	struct My_User_Regs_struct *s;
+	struct user_fpsimd_state newstate;
+	struct task_struct *target;
+
+	if (bp->attr.sample_regs_user < 0 || bp->attr.sample_regs_user >= 600) {
+		pr_info("sample_hbp_handler: invalid index\n");
+		return;
+	}
+
+	s = &my_hw_struct[bp->attr.sample_regs_user].r;
+
+	for (i = 0; i < 32; i++) {
+		if (s->vregs_set[i]) {
+			do_vregs_set = true;
+			break;
+		}
+	}
+	if (s->fpsr_set) {
+		do_vregs_set = true;
+	}
+	if (s->fpcr_set) {
+		do_vregs_set = true;
+	}
+
+	if (do_vregs_set) {
+		target = current;
+		sve_sync_to_fpsimd(target);
+		newstate = target->thread.uw.fpsimd_state;
+		for (i = 0; i < 32; i++) {
+			if (s->vregs_set[i]) {
+				newstate.vregs[i] = s->vregs[i];
+			}
+		}
+		if (s->fpsr_set) {
+			newstate.fpsr = s->fpsr;
+		}
+		if (s->fpcr_set) {
+			newstate.fpcr = s->fpcr;
+		}
+
+		target->thread.uw.fpsimd_state = newstate;
+
+		sve_sync_from_fpsimd_zeropad(target);
+		fpsimd_flush_task_state(target);
+	}
+
+	for (i = 0; i < 31; i++) {
+		if (s->xregs_set[i]) {
+			regs->regs[i] = s->xregs[i];
+		}
+	}
+	if (s->sp_set) {
+		regs->sp = s->sp;
+	}
+	if (s->pstate_set) {
+		regs->pstate = s->pstate;
+	}
+	if (s->pc_set) {
+		regs->pc = s->pc;
+	} else {
+		regs->pc += 4;
+	}
+}
+
+static int my_register_breakpoint(struct perf_event **p_sample_hbp, pid_t pid,
+				  u64 addr, u64 len, u32 type, u32 given_index)
+{
+	struct perf_event_attr attr;
+	struct task_struct *task;
+
+	task = my_get_task_struct_by_pid(pid);
+	if (!task)
+		return -1;
+
+	//hw_breakpoint_init(&attr);
+	ptrace_breakpoint_init(&attr);
+
+	attr.bp_addr = addr;
+	attr.bp_len = len;
+	attr.bp_type = type;
+	attr.disabled = 0;
+	attr.sample_regs_user = given_index;
+	*p_sample_hbp = perf_event_create_kernel_counter(
+		&attr, -1, task, sample_hbp_handler, NULL);
+
+	put_task_struct(task);
+
+	if (IS_ERR((void __force *)*p_sample_hbp)) {
+		int ret = PTR_ERR((void __force *)*p_sample_hbp);
+		pr_info(KERN_INFO "register_user_hw_breakpoint failed: %d\n",
+			ret);
+		*p_sample_hbp = NULL;
+		return ret;
+	}
+	return 0;
+}
+static int my_register_breakpoint_mult(struct perf_event **p_sample_hbp,
+				       pid_t pid, u64 addr, u64 len, u32 type,
+				       u32 given_index)
+{
+	struct task_struct *thread_group = NULL;
+	struct task_struct *task;
+	struct task_struct *pos;
+	int count = 0;
+	int tid;
+
+	task = my_get_task_struct_by_pid(pid);
+	if (!task)
+		return -1;
+
+	thread_group = task->group_leader;
+	do {
+		if (count >= MAX_HBP_COUNT) {
+			break;
+		}
+		tid = pos->pid;
+		my_register_breakpoint(&p_sample_hbp[count], tid, addr, len,
+				       type, given_index);
+		count++;
+	}
+	while_each_thread(thread_group, pos);
+	return 0;
+}
+static int my_unregister_breakpoint(struct perf_event *p_sample_hbp)
+{
+	if (p_sample_hbp) {
+		perf_event_release_kernel(p_sample_hbp);
+		return 0;
+	}
+	return -1;
+}
+
 static int dispatch_open(struct inode *node, struct file *file)
 {
 	return 0;
@@ -329,20 +488,22 @@ static int dispatch_close(struct inode *node, struct file *file)
 static long dispatch_ioctl(struct file *const file, unsigned int const cmd,
 			   unsigned long const arg)
 {
-	struct memory_args {
-		pid_t pid;
-		uintptr_t addr;
-		void *buffer;
-		size_t size;
-		size_t *ret;
-	} args;
+	int i, j;
 	size_t ret_value;
 
 	switch (cmd) {
 	case 1:
 	case 2:
 	case 3:
-	case 4:
+	case 4: {
+		struct {
+			size_t pid;
+			uintptr_t addr;
+			void *buffer;
+			size_t size;
+			size_t *ret;
+		} args;
+
 		if (copy_from_user(&args, (void *)arg, sizeof(args)))
 			return -EFAULT;
 
@@ -360,8 +521,78 @@ static long dispatch_ioctl(struct file *const file, unsigned int const cmd,
 				args.pid, args.addr, args.buffer, args.size);
 		if (copy_to_user(args.ret, &ret_value, sizeof(ret_value)))
 			return -EFAULT;
-		
-		break;
+
+	} break;
+	case 101:
+	case 102:
+	case 103: {
+		struct My_HW_struct *hw_struct;
+		struct {
+			size_t pid;
+			uintptr_t addr;
+			u64 len;
+			u64 type;
+			u64 mode;
+			u64 index;
+			size_t *ret;
+		} hb_args;
+
+		if (copy_from_user(&hb_args, (void *)arg, sizeof(hb_args)))
+			return -EFAULT;
+
+		if (cmd == 101) {
+			if (current_index >= MAX_INDEX_COUNT ||
+			    current_index < 0) {
+				current_index = 0;
+			}
+
+			hw_struct = &my_hw_struct[current_index];
+
+			memset(hw_struct, 0, sizeof(struct My_HW_struct));
+
+			if (hb_args.mode == 0) {
+				ret_value = my_register_breakpoint(
+					&my_hw_struct[current_index].hbp[0],
+					hb_args.pid, hb_args.addr, hb_args.len,
+					hb_args.type, current_index);
+
+			} else {
+				ret_value = my_register_breakpoint_mult(
+					&my_hw_struct[current_index].hbp[0],
+					hb_args.pid, hb_args.addr, hb_args.len,
+					hb_args.type, current_index);
+			}
+			if (ret_value == 0) {
+				ret_value = current_index;
+				current_index++;
+			}
+		} else if (cmd == 102) {
+			for (i = 0; i < MAX_HBP_COUNT; i++) {
+				if (my_hw_struct[hb_args.index].hbp[i]) {
+					my_unregister_breakpoint(
+						my_hw_struct[current_index]
+							.hbp[i]);
+				}
+			}
+			memset(&my_hw_struct[hb_args.index], 0,
+			       sizeof(struct My_HW_struct));
+			ret_value = 0;
+		} else if (cmd == 103) {
+			for (i = 0; i < MAX_INDEX_COUNT; i++) {
+				for (j = 0; j < MAX_HBP_COUNT; j++) {
+					if (my_hw_struct[i].hbp[j]) {
+						my_unregister_breakpoint(
+							my_hw_struct[i].hbp[j]);
+					}
+				}
+			}
+			memset(my_hw_struct, 0, sizeof(my_hw_struct));
+		}
+
+		if (copy_to_user(hb_args.ret, &ret_value, sizeof(ret_value)))
+			return -EFAULT;
+	} break;
+
 	default:
 		return -EINVAL;
 	}
